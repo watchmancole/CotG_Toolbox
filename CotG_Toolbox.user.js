9ba12f6cb6c010719cb9cfa7dbfcf9f4dcdbc446
Greg Cole <greg.cole@gmail.com>
2020-12-30 14:23:05 -0500

    Forgot to bump version.

--- CotG_Toolbox.user.js
+++ CotG_Toolbox.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name Cotg Toolbox
 // @namespace https://github.com/watchmancole/CotG_Toolbox
-// @version 1.0.12
+// @version 1.0.13
 // @description A toolbox of helper tools for the game Crown of the Gods.
 // @author WatchmanCole, Dhruv, Cfunky
 // @match https://*.crownofthegods.com
@@ -14,7 +14,7 @@
 
 (function () {
     // popup message for players when they open the game.
-     // var tbVersion = "1.0.12";
+     // var tbVersion = "1.0.13";
     // $(function () {
     //     var popwin = "<div id='HelloWorld' style='width:400px;background-color: #E2CBAC;-moz-border-radius: 10px;-webkit-border-radius: 10px;border-radius: 10px;border: 4px ridge #DAA520;position:absolute;right:40%;top:100px; z-index:1000000;'><div class=\"popUpBar\"> <span class=\"ppspan\">CotG_Toolbox v." + tbVersion + "</span><button id=\"cfunkyX\" onclick=\"$('#HelloWorld').remove();\" class=\"xbutton greenb\"><div id=\"xbuttondiv\"><div><div id=\"centxbuttondiv\"></div></div></div></button></div><div id='hellobody' class=\"popUpWindow\"><span style='margin-left: 5%;'> <h3 style='text-align:center;'>Welcome to Crown Of The Gods!</h3></span><br><br><span style='margin-left: 5%;'> <h4 style='text-align:center;'> Cotg Toolbox (Cfunky + Dhruv's Raiding helper with fixes from WatchmanCole)</h4></span><br><span style='margin-left: 5%;'><h4>changes:</h4> <ul style='margin-left: 6%;'><li>Adjusted troop raid count (2020-03-02)</li><li>Fixed so it would work on worlds 17, 18, 19 (2020-2-19)</li><li>Fixed Boss Gladiator from old GM Gordy (2020-2-19)</li><li>Renamed to CotG Toolbox (2020-2-19)</li></ul></span></div></div>";
     //     $("body").append(popwin);
@@ -3256,7 +3256,7 @@
                         carry_percentage(total_loot);
                     }
                 });
-                
+
                 $("#raid115").click(function () {
                     var i = 0;
                     var home_loot = 0;
